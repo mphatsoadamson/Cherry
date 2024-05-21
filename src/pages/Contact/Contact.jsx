113b81ba12c304131ab036f32dc4707a5c6820f3
mphatsodamson <mphaadamson@gmail.com>
2024-05-21 10:07:23 +0200

    added contact form

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -11,20 +11,36 @@ export default function Contact(){
         <h1>Welcome to Our Contact Page</h1>
         <p>Need Help? We're Here for You! Get in Touch with Our Support Team</p>
       </header>
-      
+        
+        <section className="head">Contact Details</section>
+
       <section className="blurbs">
+        <div className="blurb1">
+          <h2>Location</h2>
+          <address><strong>Zomba, University Of Malawi</strong></address>
+        </div>
+        <div className="blurb2">
+          <h2>Phone Number</h2>
+          <address>
+            <strong>+265 (0) 999 100 888</strong>
+            <br />
+            <strong>+265 (0) 888 100 888</strong>
+          </address>
+        </div>
+        <div className="blurb3">
+          <h2>Email Address</h2>
+          <address>
+            <strong>admin@cherryUnima.com</strong>
+            <br />
+            <strong>helpcentre@cherryUnima.com</strong>
+          </address>
+        </div>
+      </section>
+      
+      <section className="cform">
 
         <div className="contactdetails">
             <h2>Get In Touch</h2>
-            <address>
-                <strong>Email: helpcentre@cherry.com</strong>
-                <br />
-                <br />
-                <p>
-                    <strong>Phone:+265 (0) 999 444 300</strong>
-                </p>
-            </address>
-            <p>Feel free to contact us for an problems your are facing on the platform. To know how to navigate throu</p>
 
         </div>
 
