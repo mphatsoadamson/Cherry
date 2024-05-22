72b723e983de8a3d85c78721c5fd308dcad5329e
SifisoChitowe <bsc-com-27-21@unima.ac.mw>
2024-05-22 11:21:15 +0200

    changed some styling

--- src/pages/CreateAccount/CreateAccount.jsx
+++ src/pages/CreateAccount/CreateAccount.jsx
@@ -8,31 +8,31 @@ export default function CreateAccount(){
 
             <form className='max-w-[400px] w-full mx-auto bg-white p-4'>
 
-                <h2 className='text-4xl font bold text-center py-6'>CHERRY SIGN UP PAGE</h2>
+                <h2 className='text-4xl text-red-500 font bold text-center py-6'>CHERRY</h2>
 
                 <div className='flex flex-col py-2'>
                     <div>Your Name(full name)</div>
-                    <input className='border p-2' type="text"/>
+                    <input className='border p-2' type="text"  placeholder="Enter full name" class="ring-1 ring-gray-300 w-full rounded-md p-4 mt-2"/>
                 </div>
 
                 <div className='flex flex-col py-2'>
                     <div>Phone Number</div>
-                    <input className='border p-2' type="text"/>
+                    <input className='border p-2' type="text"  placeholder="Enter phone number" class="ring-1 ring-gray-300 w-full rounded-md p-4 mt-2"/>
                 </div>
 
                 <div className='flex flex-col py-2'>
                     <div>Email</div>
-                    <input className='border p-2' type="email"/>
+                    <input className='border p-2' type="email"  placeholder="Enter email" class="ring-1 ring-gray-300 w-full rounded-md p-4 mt-2"/>
                 </div>
 
                 <div className='flex flex-col py-2'>
                     <div>Password</div>
-                    <input className='border p-2' type="password"/>
+                    <input className='border p-2' type="password"  placeholder="Enter password" class="ring-1 ring-gray-300 w-full rounded-md p-4 mt-2"/>
                 </div>
 
                 <div className='flex flex-col py-2'>
                     <div>Confirm Password</div>
-                    <input className='border p-2' type="password"/>
+                    <input className='border p-2' type="password"  placeholder="Enter confirm password" class="ring-1 ring-gray-300 w-full rounded-md p-4 mt-2"/>
                 </div>
 
                 <button className='border w-full my-5 py-2 bg-red-600 hover:bg-red-500 text-white'>Create Account</button>
