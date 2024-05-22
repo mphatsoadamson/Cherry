bb818447de6f0680770d08207d02b4ebbc375e07
MELISSAHZULU <bsc-04-22@unima.ac.mw>
2024-05-22 22:43:33 +0200

    added heading to guideline page

--- src/pages/Guidelines/Guidelines.jsx
+++ src/pages/Guidelines/Guidelines.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Guidelines(){
     return(
-        <div>
-           Guide
+        <div className=" w-[50%] m-auto mt-3">
+            <div className=" text-center"> 
+                <p className=" p-8">Last updated 3 may 2024</p>
+                <h1 className=" font-bold text-[2.6rem]">Guidelines of Use</h1>
+            </div>
         </div>
     )
 }
