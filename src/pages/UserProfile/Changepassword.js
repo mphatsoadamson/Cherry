06abe6d3b0307c0944f48cda2e6a8d9feefeb47c
“dumi299” <bsc-com-14-21@unima.ac.mw>
2024-05-23 12:06:55 +0200

    add

--- src/pages/UserProfile/Changepassword.js
+++ src/pages/UserProfile/Changepassword.js
@@ -3,7 +3,8 @@ import React from 'react'
 const Changepassword = () => {
 
   return (
-        <div>Password Change
+    <div className= 'flex flex-col items-center w-full'>
+    <h1 className = 'text-crimson text-[30px] font-[350] py-[5px] px-[10px] mt-2.5'>Change Password</h1>
           </div>
   )
 }
--- src/pages/UserProfile/MyProducts.js
+++ src/pages/UserProfile/MyProducts.js
@@ -3,9 +3,10 @@ import React, {useState} from 'react'
 const MyProducts = () => {
 
     return (
-      <div>My Products
+      <div> 
+        <h1 className = 'flex justify-center text-crimson text-[30px] font-[350] py-[5px] px-[10px] mt-2.5'>Your Produtcs</h1>
         
-        </div>
+      </div>
   )
 }
 
